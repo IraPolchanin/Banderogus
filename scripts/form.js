75ce3885af5e87546bdd38d629f52267d185a002
Iryna Polchaninova <ira.polchanin@gmail.com>
2022-10-25 14:41:39 +0300

    added amimation

--- scripts/form.js
+++ scripts/form.js
@@ -32,20 +32,47 @@ closeBtns.forEach(btn => {
     })
 })
 
+function clearFormFields() {
+    const modalFiends = formModal.querySelectorAll('input');
+
+    modalFiends.forEach( field => { 
+        field.value = ''
+    });
+}
+
+function showGooseAnim() {
+    const targetContainer = document.querySelector('.modal-form');
+    const gusImage = document.createElement('img');
+    gusImage.setAttribute('src', './img/gus-anim.gif');
+    gusImage.classList.add('gus-anim');
+
+    targetContainer.appendChild(gusImage);
+
+    setTimeout(2000, () => {
+        targetContainer.removeChild(gusImage);
+    })
+}
+
 
 form.addEventListener('submit', e => {
     e.preventDefault();
     const formData = new FormData(form);
 
     fetch("/", {
-        method: "POST",
-        headers: { "Content-Type": "application/x-www-form-urlencoded" },
-        body: new URLSearchParams(formData).toString(),
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: new URLSearchParams(formData).toString(),
     })
-        .then(() => {
+      .then(() => {
+        showGooseAnim();
+
+        setTimeout(() => {
             closeFormModal();
             setTimeout(openSuccessModal, 700);
-            setTimeout(closeSuccessModal, 3000);
-        })
-        .catch((error) => console.log('Sending form failed'));
+            setTimeout(closeSuccessModal, 4000);
+            clearFormFields();
+        }, 4000);
+      })
+      .catch((error) => console.log('Sending form failed'));
 })
+
